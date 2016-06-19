877787d312f37bfbc04937155193bc8b832837f8
sinscary <nbsharma@outlook.com>
2016-06-19 16:13:11 +0530

    Add friendship and friend search functionality

--- db/migrate/20160619082003_create_friendships.rb
+++ db/migrate/20160619082003_create_friendships.rb
@@ -1,6 +1,7 @@
 class CreateFriendships < ActiveRecord::Migration
   def change
     create_table :friendships do |t|
+      t.belongs_to :user
       t.belongs_to :friend, class: 'User'
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@
 ActiveRecord::Schema.define(version: 20160619082003) do
 
   create_table "friendships", force: :cascade do |t|
+    t.integer  "user_id"
     t.integer  "friend_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
