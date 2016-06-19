e169274cc80e46fd7073e0dadee3af12760272cf
sinscary <nbsharma@outlook.com>
2016-06-19 13:54:06 +0530

    Add friendship model

---
+++ db/migrate/20160619082003_create_friendships.rb
@@ -0,0 +1,8 @@
+class CreateFriendships < ActiveRecord::Migration
+  def change
+    create_table :friendships do |t|
+      t.belongs_to :friend, class: 'User'
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160619071426) do
+ActiveRecord::Schema.define(version: 20160619082003) do
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer  "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "stocks", force: :cascade do |t|
     t.string   "ticker"
